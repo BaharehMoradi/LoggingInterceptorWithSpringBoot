8d96c75e8b28dcaaeb8c704f9cf755f1d7fe253f
Bahareh Moradi <b.m_89@yahoo.com>
2023-08-21 10:06:10 +0330

    Slight changes and add some comments

--- src/main/java/com/javainuse/interceptor/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/interceptor/SpringBootHelloWorldApplication.java
@@ -3,10 +3,15 @@ package com.javainuse.interceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// This annotation is a combination of annotations: @Configuration, @EnableAutoConfiguration, and @ComponentScan.
 @SpringBootApplication
+//This is the main class of your Spring Boot application.
 public class SpringBootHelloWorldApplication {
 
-	public static void main(String[] args) {
+//	It takes an array of String arguments (args) that can be used to pass command-line arguments to your application.
+		public static void main(String[] args) {
+
+//		It initializes and configures the Spring context, performs auto-configuration, and starts the embedded servlet container
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 }
--- src/main/java/com/javainuse/interceptor/controller/HelloController.java
+++ src/main/java/com/javainuse/interceptor/controller/HelloController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Define a rest controller class
 public class HelloController {
 
+//	Initialize a logger instance using the SLF4J (Simple Logging Facade for Java) framework
 	Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping("/hello")
+	//The @RequestMapping annotation, when used without specifying the method attribute, will match HTTP GET requests by default
 	public String displayHello() {
+		//log information to the
 		log.info("inside the displayHello method");
+		//Implement the business logic
 		return "Hello World!";
 	}
 }
