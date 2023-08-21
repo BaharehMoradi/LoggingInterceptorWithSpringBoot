a530d642b1d4f1c81f4755412ffb2e16cd0677d1
Bahareh Moradi <b.m_89@yahoo.com>
2023-08-21 13:45:29 +0330

    intercept specific controller methods annotated with @Loggable

--- src/main/java/com/javainuse/interceptor/config/LoggerInterceptor.java
+++ src/main/java/com/javainuse/interceptor/config/LoggerInterceptor.java
@@ -3,6 +3,7 @@ package com.javainuse.interceptor.config;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.javainuse.interceptor.annotations.Loggable;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
@@ -14,32 +15,33 @@ import java.lang.reflect.Method;
 @Component
 public class LoggerInterceptor implements HandlerInterceptor {
 
-	Logger log = org.slf4j.LoggerFactory.getLogger(this.getClass());
+	Logger logger_Interceptor = org.slf4j.LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public void afterCompletion(HttpServletRequest request,
-			HttpServletResponse response, Object object, Exception arg3)
+								HttpServletResponse response, Object object, Exception arg3)
 			throws Exception {
-		log.info("Request is complete");
+		logger_Interceptor.info("logger_Interceptor: Request is complete");
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request,
-			HttpServletResponse response, Object object, ModelAndView model)
+						   HttpServletResponse response, Object object, ModelAndView model)
 			throws Exception {
-		log.info("Handler execution is complete");
+		logger_Interceptor.info("logger_Interceptor: Handler execution is complete");
 	}
 
-@Override
-public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-	if (handler instanceof HandlerMethod) {
-		HandlerMethod handlerMethod = (HandlerMethod) handler;
-		Method method = handlerMethod.getMethod();
-		String className = handlerMethod.getBeanType().getSimpleName();
-		String methodName = method.getName();
-		log.info("Before Handler execution - Class: {}, Method: {}", className, methodName);
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			Method method = handlerMethod.getMethod();
+			if (method.isAnnotationPresent(Loggable.class)) {
+				String className = handlerMethod.getBeanType().getSimpleName();
+				String methodName = method.getName();
+				logger_Interceptor.info("logger_Interceptor: Before Handler execution - Class: {}, Method: {}", className, methodName);
+			}
+		}
+		return true;
 	}
-	return true;
-}
-
 }
--- src/main/java/com/javainuse/interceptor/controller/HelloController.java
+++ src/main/java/com/javainuse/interceptor/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.javainuse.interceptor.controller;
 
+import com.javainuse.interceptor.annotations.Loggable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 //	Initialize a logger instance using the SLF4J (Simple Logging Facade for Java) framework
-	Logger log = LoggerFactory.getLogger(this.getClass());
+	Logger logger_HelloController = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping("/hello")
 	//The @RequestMapping annotation, when used without specifying the method attribute, will match HTTP GET requests by default
 	public String displayHello() {
 		//log information to the
-		log.info("inside the displayHello method");
+		logger_HelloController.info("logger_HelloController: inside the displayHello method");
+		//Implement the business logic
+		return "Hello!";
+	}
+	@Loggable
+	@RequestMapping("/helloworld")
+	//The @RequestMapping annotation, when used without specifying the method attribute, will match HTTP GET requests by default
+	public String displayHelloWorld() {
+		//log information to the
+		logger_HelloController.info("logger_HelloController: inside the displayHelloWorld method");
 		//Implement the business logic
 		return "Hello World!";
 	}
+
 }
